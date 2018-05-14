616807fac45aaf2e041bd97bf1dd59519fbeee51
Tanvir <38818306+tmonzur@users.noreply.github.com>
2018-05-14 21:29:27 +0600

    Update db.php

--- lib/db.php
+++ lib/db.php
@@ -8,23 +8,20 @@ function post_setup_status_data(){
 	$sassignee = $_POST['assignee'];
 	$snotes = $_POST['notes'];
 	$pageTitle = $_POST['page_title'];
+	$dataId =  $_POST['uid'];
 		
 	if( isset( $_POST['ajaxrequest'] ) && $_POST['ajaxrequest'] === 'true' ) {
 		global $wpdb;
 		
 		$current_url = $_SERVER[HTTP_REFERER];
 				
-			//$sql = "SELECT * FROM ".$wpdb->prefix."setup_status_info";
-			//$results = $wpdb->get_results($sql);
-			//$pageTitle = get_admin_page_title();
+			if ($dataId>0){ 
+				$wpdb->update( $wpdb->prefix.'setup_status_info', array('setup_status' => $sstatus, 'assignee' => $sassignee, 'status_note' => $snotes, 'issue_date' => current_time( 'mysql' )), array( 'id'=>$dataId));
+				echo "2";
+			}else{
+				$wpdb->insert( $wpdb->prefix.'setup_status_info', array("setup_status" => $sstatus, "assignee" => $sassignee, "status_note" => $snotes, "page_name" => $pageTitle, "page_url" => $current_url, "issue_date" => current_time( 'mysql' )));					
+			}
 		
-			//$table_name = $wpdb->prefix . 'newsletter';
-			//if (count($results)==0 || count($results)== ''){ 
-			$wpdb->insert( $wpdb->prefix.'setup_status_info', array("setup_status" => $sstatus, "assignee" => $sassignee, "status_note" => $snotes, "page_name" => $pageTitle, "page_url" => $current_url, "issue_date" => current_time( 'mysql' )));					
-			//}else{
-			//	$wpdb->update( $wpdb->prefix.'setup_status_info', array('setup_status' => $sstatus, 'assignee' => $sassignee, 'status_note' => $snotes, 'issue_date' => current_time( 'mysql' )), array( 'id'=>1));
-			//}
-			//var_dump( $wpdb->last_query );
 		die();
 		return true;
 	}		
